7455d2bcf1ef51b74955d313ae7110d03a075483
Burbietta <46001963+Burbietta@users.noreply.github.com>
2019-11-25 15:39:39 +0200

    mda

--- screens/PostDetails.js
+++ screens/PostDetails.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Text, StyleSheet, Button, View, Alert } from 'react-native';
+ 
 
 
 const PostDetails = () => {
     return (<Text>tare rau</Text>)
 
 
-
+    
 
 }
 const styles = StyleSheet.create({
