330e7914e47d594577121167a232962e220f310b
Serban Jinga <mihaiserbanjinga@gmail.com>
2019-11-28 23:26:37 +0200

    Alt

---
+++ core/config.js
@@ -0,0 +1,10 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBgMZYxYnU9HMYov0z4LOo0AtVqGX32doY",
+  authDomain: "proiectscoala-d9dd3.firebaseapp.com",
+  databaseURL: "https://proiectscoala-d9dd3.firebaseio.com",
+  projectId: "proiectscoala-d9dd3",
+  storageBucket: "proiectscoala-d9dd3.appspot.com",
+  messagingSenderId: "72916424034",
+  appId: "1:72916424034:web:39fcc57a485e4438cf483b" 
+};
