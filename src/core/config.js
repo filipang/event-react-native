8c0489373717665e2a847932e3be9a8f8c402bff
Serban Jinga <mihaiserbanjinga@gmail.com>
2019-12-17 22:20:37 +0200

    mama

---
+++ src/core/config.js
@@ -0,0 +1,11 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBZ5GWPcuhxxjG1EgwNZtLgMbNrL1HuS5E",
+  authDomain: "event-dd4b9.firebaseapp.com",
+  databaseURL: "https://event-dd4b9.firebaseio.com",
+  projectId: "event-dd4b9",
+  storageBucket: "event-dd4b9.appspot.com",
+  messagingSenderId: "93847099331",
+  appId: "1:93847099331:web:f372ebcb02ed886e3d0f76",
+  measurementId: "G-CCZ9SELPCJ"
+};
---
+++ src/screens/Dashboard.js
@@ -0,0 +1,125 @@
+import React, { memo, useState, useEffect } from "react";
+import Background from "../components/Background";
+import { FlatList, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Chip } from 'react-native-paper';
+import { Tag } from '../components/Tag';
+
+
+const tagSelectorData = [
+  {
+    id: '1',
+    title: 'Manele',
+    selected: 'false'
+  },
+  {
+  id: '2',
+  title: 'Rock',
+  selected: 'false'
+  },
+  {
+    id: '3',
+    title: 'Taranie',
+    selected: 'false'
+
+  },
+  {
+  id: '4',
+  title: 'Info',
+  selected: 'false'
+
+  },{
+    id: '5',
+    title: 'Scoala',
+    selected: 'false'
+
+  },
+  {
+  id: '6',
+  title: 'Betie',
+  selected: 'false'
+
+  },{
+    id: '7',
+    title: 'Parc',
+    selected: 'false'
+
+  },
+  {
+  id: '8',
+  title: 'Picnick',
+  selected: 'true'
+  },
+];
+
+
+
+
+const Dashboard = () => {
+  const [results, setResults] = useState([]);
+  const makeRequest = () => {
+    const response = true;
+    setResults(response);
+  }
+  useEffect(() => {
+    makeRequest()
+  }, [])
+
+  
+  
+return(
+  
+<View style={styles.container}>
+    <Text style={styles.text}>What do you like ?</Text>
+    <TextInput
+    numberOfLines={1}
+    style={styles.input}
+    placeholder="Search here"
+    ></TextInput>
+    <View style={styles.viewGray}></View>
+    <FlatList
+    numColumns={3}
+     style={styles.flatlist} data={tagSelectorData}
+    renderItem={({ item }) => <Tag/>}
+    keyExtractor={item => item.id}
+    />
+</View>) 
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%"
+  },
+  viewGray:{
+    backgroundColor: '#f0f0f0',
+    height: 1,
+    width: "100%",
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 10
+  },
+  input: {
+    marginTop: 26,
+    marginLeft: 20,
+    alignContent: 'flex-start',
+  },
+  text: {
+    marginLeft: 20,
+    marginTop: 60,
+    fontSize: 16
+  },
+  flatlist: {
+    marginLeft: 20,
+    marginTop: 40,
+  },
+  flatlist_items:{
+    marginRight: 12,
+    width: 100,
+    height: 32,
+    marginTop: 8,
+    alignContent: 'flex-start',
+  }
+})
+
+export default memo(Dashboard);
