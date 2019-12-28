87a554490579410340501d22c9fd67f87c9fcc3f
Burbietta <46001963+Burbietta@users.noreply.github.com>
2019-12-28 16:58:42 +0200

    Update CalendarScreen.js

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,20 +1,7 @@
 import React from 'react';
-import { Text, StyleSheet, Dimensions, View, Alert, Image, ScrollView } from 'react-native';
-const { height, width } = Dimensions.get('window');
-import { database } from '../App';
 import CalendarPicker from '../components/CalendarPicker';
 const CalendarScreen = () => {
     return <CalendarPicker/>
-
 }
 
-
-const styles = StyleSheet.create({
-
-    imageStyle: {
-        height: 400,
-        width: width,
-        resizeMode: 'stretch',
-    },
-});
 export default CalendarScreen;
